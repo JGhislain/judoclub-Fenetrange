5e2721456f07e2870630c955ff91d5aae1174324
Ghislain <ghislain.jambert@gmail.com>
2022-01-26 04:17:01 +0100

    ajout des modales (contact + mentions)

--- js/results.js
+++ js/results.js
@@ -40,3 +40,27 @@ let afficheTableau = function() {
 }
 
 afficheTableau()
+
+//--------------------------------------------------------------------------------------//
+//                   Appel du DOM pour fenetre modal mentions légales                   //
+//--------------------------------------------------------------------------------------//
+
+const modalDisplayLegal = document.querySelector(".content-modal-legal")
+const modalLegalOpen = document.querySelector(".modal-legal")
+const modalLegalClose = document.querySelector('.modal__close__legal')
+
+//--------------------------------------------------------------------------------------//
+//                       Fonction d'ouverture de la fenêtre modal                       //
+//--------------------------------------------------------------------------------------//
+modalLegalOpen.addEventListener('click', (e) => {
+    e.preventDefault()
+    modalDisplayLegal.classList.add('modal-show')
+    closeLegal()
+})
+
+function closeLegal () {
+    modalLegalClose.addEventListener('click', (e) => {
+        e.preventDefault()
+        modalDisplayLegal.classList.remove('modal-show')
+    })
+}
